42ede5086abb2b0dbf4e5f15dd7895b76960756b
maxvrt <v.onrut@gmail.com>
2022-06-10 11:20:35 +0300

    правки по ревью1

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect}  from 'react';
+import React, {useEffect, useState}  from 'react';
 import AppHeader from '../app-header/App-header';
 import BurgerIngredients from '../burger-ingredients/Burger-ingredients';
 import BurgerConstructor from '../burger-constructor/Burger-constructor';
@@ -6,18 +6,13 @@ import IngredientDetails from '../ingredient-details/ingredient-details';
 import OrderDetails from '../order-details/order-details';
 import Modal from '../modal/Modal';
 import app from './app.module.css';
+import config from '../../utils/config';
 
 function App() {
-  const config = {
-    baseUrl: "https://norma.nomoreparties.space/api/ingredients",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
-  const [array, setArray] = React.useState([]);
-  const [isOrderDetailsOpened, setIsOrderDetails] = React.useState(false);
-  const [ingredientDetailsOpened, setIsIngredientDetails] = React.useState(false);
-  const [ingredient, setIngredient] = React.useState({});
+  const [array, setArray] = useState([]);
+  const [isOrderDetailsOpened, setIsOrderDetails] = useState(false);
+  const [ingredientDetailsOpened, setIsIngredientDetails] = useState(false);
+  const [ingredient, setIngredient] = useState({});
   // Закрытие всех модалок
   const closeAllModals = () => {
     setIsOrderDetails(false);
@@ -33,7 +28,6 @@ function App() {
     setIngredient(item);
     setIsIngredientDetails(true);
   }
-
 
   useEffect(() => {
     fetch(`${config.baseUrl}`, {headers: config.headers})
---
+++ src/utils/config.js
@@ -0,0 +1,7 @@
+const config = {
+  baseUrl: "https://norma.nomoreparties.space/api/ingredients",
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
+export default config
